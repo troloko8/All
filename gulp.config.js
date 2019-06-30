338931cc3612856c2fdf6753356ac4d5e25193c8
Tytarenko Anatolii <troloko8@gmail.com>
2019-06-30 20:26:10 +0300

    ...

---
+++ gulp.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  SRC_PATH: "profile",
+  DIST_PATH: 'profileDev',
+  STYLES_LIBS: ['node_modules/normalize.css/normalize.css'],
+  JS_LIBS: [
+    'node_modules/jquery/dist/jquery.js',
+    'node_modules/jquery-touchswipe/jquery.touchSwipe.min.js'
+  ]
+}
